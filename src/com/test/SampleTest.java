33396b70c6a396529dadc3819d78155c0efaee3d
sridharkasi <sridharan.kasi@gmail.com>
2017-04-23 19:04:27 +0530

    new changes

--- src/com/test/SampleTest.java
+++ src/com/test/SampleTest.java
@@ -5,7 +5,7 @@ public class SampleTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	System.out.println("Welocme");
+	System.out.println("Welocme1");
 	}
 
 }
